9b5d07b8f1087af1f9328ccdebbaa8be13b4afe4
Gabriel MB <gabrielbaraglio@outlook.com.br>
2025-06-07 18:20:57 -0300

    12.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "12.4"
+        versionName = "12.5"
         buildConfigField("String", "EAD_URL", "\"${project.properties["EAD_URL"]}\"")
         buildConfigField("String", "GITHUB_PAT", "\"${project.properties["GITHUB_PAT"]}\"")
     }
