b3b602eb3ecaf77be504cd5d95855ce7bb4d4f05
Gabriel MB <gabrielbaraglio@outlook.com.br>
2025-07-01 17:35:33 -0300

    18.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "18.0"
+        versionName = "18.1"
         buildConfigField("String", "EAD_URL", "\"${project.properties["EAD_URL"]}\"")
         buildConfigField("String", "GITHUB_PAT", "\"${project.properties["GITHUB_PAT"]}\"")
     }
